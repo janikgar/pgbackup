60d8768a283f164fc882bf2b0db7939ef7ac8dbf
J. Garcia <janikgar@gmail.com>
2022-07-02 20:29:06 -0700

    fix up entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM postgresql:16.4-bookworm as source
-
 FROM minio/mc:latest
 
-COPY --from=source /usr/bin/pg_dumpall /usr/bin/pg_dumpall
+COPY --from=source /usr/bin/mc /usr/bin/mc
 COPY entrypoint.sh /
 
 ENTRYPOINT ["/bin/bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 #!/usr/bin/env sh
 
 cd /tmp
 
 PG_HOST=${PG_HOST:-postgres.home.lan}
-=======
-#!/usr/bin/env bash
-
-cd /tmp
-
-PG_HOST=${PG_HOST:-postgresql14.postgresql14.svc.cluster.local}
->>>>>>> 6fa876b (initial commit)
 PG_USER=${PG_USER:-postgres}
 PG_PASSWORD=${PG_PASSWORD:-postgres}
 PG_PORT=${PG_PORT:-5432}
 
 echo "${PG_HOST}:${PG_PORT}:*:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass
-<<<<<<< HEAD
+
 chmod 0600 ~/.pgpass
 
 DATETIME=$(date -Iminutes)
@@ -25,7 +17,3 @@ pg_dumpall -f "backup-${DATETIME}.sql" -h ${PG_HOST} -U ${PG_USER}
 gzip backup-${DATETIME}.sql
 
 mc cp backup-${DATETIME}.sql.gz syno/postgres-backups
-=======
-
-pg_dumpall -f "backup-$(date -Iminutes).sql"
->>>>>>> 6fa876b (initial commit)
